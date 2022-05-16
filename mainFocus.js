0c674454db7d72f17ca960867d22168f251ec9b4
Garima <kumargarima59@gmail.com>
2022-05-16 11:26:31 +0530

    feat: weather, quote, main task to focus

--- mainFocus.js
+++ mainFocus.js
@@ -1,3 +1,19 @@
+const enterMainTask = document.querySelector(".enterMainTask");
+const focusTask = document.querySelector(".focusTask");
+
 const displayTask = document.querySelector(".display");
 
+let savedTask = localStorage.getItem("maintask");
+focusTask.innerText = savedTask;
+
+const addFocusTask = (event) => {
+  console.log(event);
+  if (event.key === "Enter") {
+    focusTask.innerText = event.target.value;
+    localStorage.setItem("maintask", event.target.value);
+  }
+};
+
 if (localStorage.getItem("userName")) displayTask.style.display = "block";
+
+enterMainTask.addEventListener("keypress", addFocusTask);
